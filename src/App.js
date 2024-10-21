75cf1f203e07dca6ff421fbf5e9b5d73a2a674a0
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-21 09:04:48 -0700

    Integrate map using google map api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const mapContainerStyle = {
-  width: '100%',
-  height: '400px',
+  width: "100%",
+  height: "400px",
 };
 
 const center = {
-  lat: ,
-  lng: ,
+  lat: 31.734739,
+  lng: 34.991741,
 };
+
+// AlzaSyNHPv2bxy4LgEwiWgUPBoB2flUM5K6FmTf  temporary api
 
 function MapComponent() {
   return (
-    <LoadScript googleMapsApiKey="">
+    <LoadScript googleMapsApiKey="AlzaSyNHPv2bxy4LgEwiWgUPBoB2flUM5K6FmTf">
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={14}
